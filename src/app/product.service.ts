2e4b015c63f7f26308d4c0d61e317c7a9cee60da
popoolaolusola92@gmail.com <popoolaolusola92@gmail.com>
2019-10-29 14:30:27 +0100

    using interface to describe data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
-
-
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
+
+import { Album } from './album';
+
 //import { map } from 'rxjs/operator/map';
 
 
@@ -13,7 +16,7 @@ export class ProductService {
 
 
   constructor(private _http: Http) { }
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => response.json())
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json())
     }
 }
