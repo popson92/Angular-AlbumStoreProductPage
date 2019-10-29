26a498a9ed5e6f52471d572f812b03b0b8344484
popoolaolusola92@gmail.com <popoolaolusola92@gmail.com>
2019-10-29 11:43:09 +0100

    service creation for the album data

---
+++ src/app/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http, Response} from '@angular/http';
+
+
+import 'rxjs/add/operator/map';
+//import { map } from 'rxjs/operator/map';
+
+
+@Injectable()
+export class ProductService {
+  private _albumUrl: string = '../assets/album.json';
+  response: any;
+
+
+  constructor(private _http: Http) { }
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) => response.json())
+    }
+}
